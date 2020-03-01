0c1085bd5c4a8cca6edf10c7074b83ebc603344c
浦崎龍成 <ura2025@urasakiryuuseinoMacBook-Air.local>
2020-03-01 16:51:31 +0900

    [start]6-5

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '896f64cb61f4c92b8b42e40e46b1fedb9692b35765a422d8b2da0d93892f3056623a96b3455cc132a2fec91ffe773dfe7b353736b8bb78561f7accf84acfd054'
