6bf6ba0bd4c576389d2de3cc1f9fc7ab6717e229
浦崎龍成 <ura2025@urasakiryuuseinoMacBook-Air.local>
2020-03-05 17:05:15 +0900

    [finishi]6-7

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,11 @@
 class PostImage < ApplicationRecord
-	belongs_to :user
-    attachment :image # ここを追加（_idは含めません）
-    has_many :post_comments, dependent: :destroy
+	attachment :image
+        belongs_to :user
+        has_many :favorites, dependent: :destroy
+        has_many :post_comments, dependent: :destroy
+        validates :shop_name, presence: true
+        validates :image, presence: true
+        def favorited_by?(user)
+        favorites.where(user_id: user.id).exists?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- root 'post_images#index'
- resources :post_images, only: [:new, :create, :index, :show]
- resource :post_comments, only: [:create]
-end
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show] do
+  	resource :post_comments, only: [:create]
+  	resource :favorites, only: [:create, :destroy]
+  end
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+	resource :favorites, only: [:create, :destroy]
+	resource :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
+
+  end
+
+
+
+
